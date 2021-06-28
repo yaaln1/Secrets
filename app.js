78fb39d074db8364c9b7062012ecd6cc77792088
Leonid <leoad@mail.ru>
2021-06-28 11:16:30 +0600

    Login and Register with md5

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 const app = express();
-const encrypt = require('mongoose-encryption');
+const md5 = require("md5");
 
 console.log(process.env.API_KEY);
 
@@ -25,8 +25,7 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,7 +44,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function (err) {
     if (!err) {
@@ -59,7 +58,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function (err, foundUser) {
     if (err) {
